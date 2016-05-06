5652789f8dfde21ca6e499309a155ef66440e0d1
Casper Bollen <halcwb@gmail.com>
2016-05-06 17:04:52 +0200

    First working version

--- src/Informedica.GenUnits.lib/Scripts/load-project-release.fsx
+++ src/Informedica.GenUnits.lib/Scripts/load-project-release.fsx
@@ -2,3 +2,4 @@
 #I __SOURCE_DIRECTORY__
 #load "load-references-release.fsx"
 #load "../Utils.fs"
+      "../Unit.fs"
--- src/Informedica.GenUnits.lib/Utils.fs
+++ src/Informedica.GenUnits.lib/Utils.fs
@@ -1,5 +1,42 @@
 ﻿namespace Informedica.GenUnits.Lib
 
+
+module StringBCL =
+
+    let toString s = s.ToString()
+
+    let empty (s: string) = System.String.IsNullOrWhiteSpace(s)
+
+    let notEmpty = empty >> not
+
+    let toUpper (s: string) = s.ToUpper()
+
+    let toLower (s: string) = s.ToLower()
+
+    let length (s: string) = s.Length
+
+    let substring start length (s: string) = s.Substring(start, length)
+
+    let trim (s: string) = s.Trim()
+
+    /// Get the first character of a string
+    /// as a string
+    let firstStringChar = substring 0 1
+
+    /// Return the rest of a string as a string
+    let restString s = substring 1 ((s |> length) - 1) s
+
+    let firstToUpper = firstStringChar >> toUpper
+
+    let capitalize s = (s |> firstToUpper) + (s |> restString |> toLower)
+
+    let letters = ['a'..'z'] @ ['A'..'Z'] |> List.map toString
+
+    let isLetter s = List.exists (fun s' -> s' = s) letters
+
+    let equalsCapInsens s1 s2 = s1 |> toLower |> trim = (s2 |> toLower |> trim) 
+
+
 module Utils =  
     
     module Nullable =

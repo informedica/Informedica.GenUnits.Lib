1214f4eec68ff390a201cb7ce02a3bcf7a2b1ad1
Casper Bollen <halcwb@gmail.com>
2016-05-11 12:01:13 +0200

    Bump version to 0.1.1-beta

--- src/Informedica.GenUnits.lib/AssemblyInfo.fs
+++ src/Informedica.GenUnits.lib/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Informedica.GenUnits.lib")>]
 [<assembly: AssemblyProductAttribute("GenUnits")>]
 [<assembly: AssemblyDescriptionAttribute("")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
-    let [<Literal>] InformationalVersion = "0.0.1"
+    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] InformationalVersion = "0.1.1"

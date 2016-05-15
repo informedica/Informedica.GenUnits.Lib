0cc9c3f8284eb9c1373d7af30de5e8e14d65732b
Casper Bollen <halcwb@gmail.com>
2016-05-15 16:41:23 +0200

    Updated refs and updated docs

--- src/Informedica.GenUnits.lib/Scripts/load-references-release.fsx
+++ src/Informedica.GenUnits.lib/Scripts/load-references-release.fsx
@@ -1,6 +1,5 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
-#r "../../../packages/FsCheck/lib/net45/FsCheck.dll"
 #r "../../../packages/FSPowerPack.Core.Community/Lib/Net40/FSharp.PowerPack.dll"
 #r "../../../packages/FSPowerPack.Linq.Community/Lib/Net40/FSharp.PowerPack.Linq.dll"
 #r "../../../packages/FSPowerPack.Metadata.Community/Lib/Net40/FSharp.PowerPack.Metadata.dll"
@@ -8,4 +7,4 @@
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.Numerics.dll"
-#r "../../../packages/Unquote/lib/net45/Unquote.dll"
+#r "../../../packages/Informedica.GenUtils.Lib/lib/net45/Informedica.GenUtils.Lib.dll"

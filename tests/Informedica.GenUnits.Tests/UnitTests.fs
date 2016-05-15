36eaf3eb6bbf11cfee71853f30339f7dc78d079c
Casper Bollen <halcwb@gmail.com>
2016-05-15 16:16:09 +0200

    Added test lib

---
+++ tests/Informedica.GenUnits.Tests/UnitTests.fs
@@ -0,0 +1,42 @@
+﻿namespace Informedica.GenUnits.Tests
+
+open Swensen.Unquote
+open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+
+open Informedica.GenUnits.Lib
+
+
+/// Create the necessary test generators
+module Generators =
+
+    let bigRGen (n, d) = 
+            let d = if d = 0 then 1 else d
+            let n' = abs(n) |> BigRational.FromInt
+            let d' = abs(d) |> BigRational.FromInt
+            n'/d'
+
+    let bigRGenerator =
+        gen {
+            let! n = Arb.generate<int>
+            let! d = Arb.generate<int>
+            return bigRGen(n, d)
+        }
+
+    type MyGenerators () =
+        static member BigRational () =
+            { new Arbitrary<BigRational>() with
+                override x.Generator = bigRGenerator }
+
+
+[<SetUpFixture>]
+type Config () =
+    
+    /// Make sure the generators are
+    /// registered before running any
+    /// test code.
+    [<SetUp>]
+    member x.Setup () = 
+
+        Arb.register<Generators.MyGenerators>() |> ignore
